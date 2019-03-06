03366ed5010f68005387505ce019faa1790bfc17
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-03-06 11:00:04 +0530

    countDown and Sum with recursion

--- src/main/Recursion.java
+++ src/main/Recursion.java
@@ -1,5 +1,25 @@
 package main;
 
 public class Recursion {
+    static void countDown(int number) {
+        if (number <= 0) {
+            return;
+        }
+        System.out.println(number);
+        --number;
+        countDown(number);
+    }
 
+    static int sum(int number) {
+        if (number == 1) {
+            return 1;
+        }
+        return number + sum(--number);
+    }
+
+    public static void main(String[] args) {
+        countDown(4);
+        System.out.println("------");
+        System.out.println(sum(4));
+    }
 }
